837cc66cffffe14281d8da786b7c26f6c4ec2ff4
Seeeev <sevenabante9@gmail.com>
2025-09-03 22:20:41 +0800

    init

---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<template></template>
+<!-- <template>
+  <div>{{ greeting }}</div>
+</template>
+
+<script setup>
+const hour = new Date().getHours()
+const greeting = hour < 12 ? 'Good morning' : 'Good afternoon'
+</script> -->
+
+<!-- <template>
+  <UApp class="responsive-content">
+    <div class="hidden md:block">Desktop content</div>
+    <div class="md:hidden">Mobile content</div>
+  </UApp>
+</template> -->
---
+++ app/pages/login.vue
@@ -0,0 +1,45 @@
+<template>
+</template>
+<!-- <template>
+  <UContainer class="flex justify-center py-10">
+    <UCard class="w-full max-w-sm">
+      <template #header>
+        <h1 class="text-xl font-bold">Login</h1>
+      </template>
+      <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
+        <UFormField label="Email" name="email">
+          <UInput v-model="state.email" class="w-full" />
+        </UFormField>
+
+        <UFormField label="Password" name="password">
+          <UInput v-model="state.password" type="password" class="w-full" />
+        </UFormField>
+
+        <UButton type="submit" class="justify-center w-full"> Submit </UButton>
+      </UForm>
+    </UCard>
+  </UContainer>
+</template>
+
+<script setup lang="ts">
+import * as z from 'zod'
+import type { FormSubmitEvent } from '@nuxt/ui'
+
+const schema = z.object({
+  email: z.string().email('Invalid email'),
+  password: z.string().min(8, 'Must be at least 8 characters'),
+})
+
+type Schema = z.output<typeof schema>
+
+const state = reactive<Partial<Schema>>({
+  email: undefined,
+  password: undefined,
+})
+
+const toast = useToast()
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  toast.add({ title: 'Success', description: 'The form has been submitted.', color: 'success' })
+  console.log(event.data)
+}
+</script> -->
